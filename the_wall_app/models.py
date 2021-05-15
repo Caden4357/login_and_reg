cdf1b664dfdb279531ba118595a7fb421aea0b9b
Caden Wilcox <wilcox4357@gmail.com>
2021-05-14 20:10:17 -0700

    working on changing password and adding dob

--- the_wall_app/models.py
+++ the_wall_app/models.py
@@ -3,15 +3,17 @@ from django.db import models
 from django.db.models.deletion import CASCADE
 from login_app.models import *
 
+
+
 class Message(models.Model):
     message = models.TextField(max_length=2000)
-    user = models.OneToOneField(User, related_name='message_owner', on_delete=CASCADE)
+    posted_by = models.ForeignKey(User, related_name='messages', on_delete=CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 class Comment(models.Model):
     comment = models.TextField(max_length=1000)
-    message = models.OneToOneField(Message, related_name='message_commited_on', on_delete=CASCADE)
-    user = models.OneToOneField(User, related_name='user_comment', on_delete=CASCADE)
+    message = models.ForeignKey(Message, related_name='comments',on_delete=CASCADE)
+    owner = models.ForeignKey(User, related_name='posted_by', on_delete=CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- the_wall_app/views.py
+++ the_wall_app/views.py
@@ -1,2 +1,71 @@
 from django.shortcuts import render, redirect
 from .models import *
+from django.contrib import messages
+
+def index(request):
+    return render(request, 'index.html')
+
+def register(request):
+    if request.method == "GET":
+        return redirect('/')
+    errors = User.objects.basic_validator(request.POST)
+    if errors:
+        for key,value in errors.items():
+            messages.error(request, value)
+        return redirect('/')
+    else:
+        user = User.objects.register(request.POST)
+        request.session['user_id'] = user.id
+        return redirect('/blog')
+
+
+def login(request):
+    if request.method == "GET":
+        return redirect('/')
+    if not User.objects.authenticate(request.POST['email_address'], request.POST['password']):
+        messages.error(request, "Invalid Email or Password")
+        return redirect('/')
+    user = User.objects.get(email_address=request.POST['email_address'])
+    request.session['user_id'] = user.id
+    return redirect('/blog')
+
+def logout(request):
+    request.session.clear()
+    return redirect('/')
+
+
+def blog_page(request):
+    if 'user_id' not in request.session:
+        return redirect('/')
+    user = User.objects.get(id=request.session['user_id'])
+    context = {
+        'user': user,
+        'messages': Message.objects.all(),
+    }
+    return render(request, 'user_page.html', context)
+
+
+def post_message(request):
+    if request.method == "GET":
+        return redirect('/')
+    Message.objects.create(
+        message = request.POST['post_message'],
+        posted_by = User.objects.get(id=request.session['user_id'])
+    )
+    return redirect('/blog')
+
+def post_comment(request, id):
+    if request.method == "GET":
+        return redirect('/')
+    message = Message.objects.get(id=id)
+    Comment.objects.create(
+        comment = request.POST['post_comment'],
+        message = message,
+        owner = User.objects.get(id=request.session['user_id'])
+    )
+    return redirect('/blog')
+
+# def delete_message(request, id):
+#     to_delete = Message.objects.get(id=id)
+#     to_delete.delete()
+#     return redirect('/blog')
